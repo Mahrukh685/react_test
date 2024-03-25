5854fe1ce37235f32e22a1b1b04c7cbaadd87e31
Mahrukh685 <mahrukhkhan685@gmail.com>
2024-03-26 03:29:10 +0500

    object

--- react_proj1/src/App.jsx
+++ react_proj1/src/App.jsx
@@ -1,13 +1,23 @@
+import Image from "./Profile";
 function App() {
   return (
     <div>
       <h1>Hello World </h1>
-      <Image />
+      <h1> {name} </h1>
+      <h1> {person.name} </h1>
+      <h1> {person.age} </h1>
+      <Image name={"maha"} />
+      <Image name={"aliza"} />
+      <Image name="hamza" />
     </div>
   );
 }
-function Image() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="img" />;
-}
+
+let name = "mahrukh";
+
+const person = {
+  name: "Mahrukh",
+  age: "22",
+};
 
 export default App;
---
+++ react_proj1/src/Profile.jsx
@@ -0,0 +1,11 @@
+function Image({name}) {
+  return (
+    <div>
+        <h1 style={{color:"blue"}}>{name}</h1>
+      <img src="https://i.imgur.com/MK3eW3As.jpg" alt="img" />
+    </div>
+    
+  );
+}
+
+export default Image;
